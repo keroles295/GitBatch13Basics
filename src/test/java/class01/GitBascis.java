f233b332a700104e211c809b34d398d7043abc24
keroles295 <kerolesbotros295@gmail.com>
2022-08-27 10:38:59 -0400

    four lines of codes

--- src/test/java/class01/GitBascis.java
+++ src/test/java/class01/GitBascis.java
@@ -3,5 +3,8 @@ package class01;
 public class GitBascis {
     public static void main(String[] args) {
         System.out.println("Today   is not  good day");
+        System.out.println("Today   is not  good day");
+        System.out.println("Today   is not  good day"); System.out.println("Today   is not  good day");
+
     }
 }
